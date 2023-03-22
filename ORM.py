2d6f7aa3b2a94966fda3658899e52bef715f1e29
OlegKumachev <117755901+OlegKumachev@users.noreply.github.com>
2023-03-22 21:12:13 +0300

    Update ORM.py

--- ORM.py
+++ ORM.py
@@ -1,10 +1,10 @@
 import json
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
-from models import create_tables
+from models import create_tables, Publisher, Shop, Book, Stock, Sale
 
 
-DSN = "postgresql://postgres:4652732-oP@localhost:5432/bookdb"
+DSN = "postgresql://postgres:postgres@localhost:5432/database"
 engine = sqlalchemy.create_engine(DSN)
 
 create_tables(engine)
@@ -12,8 +12,44 @@ create_tables(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-with open('fixtures/tests_data.json', 'r') as fd:
-    data = json.load(fd)
-    print(data)
+
+def load_json():
+    with open('tests_data.json') as fd:
+        data = json.load(fd)
+    for record in data:
+        model = {
+            'publisher': Publisher,
+            'shop': Shop,
+            'book': Book,
+            'stock': Stock,
+            'sale': Sale,
+        }[record.get('model')]
+
+        session.add(model(id=record.get('pk'), **record.get('fields')))
+    session.commit()
+
+
+def get_data():
+    id_name = input('Введите данные издателя: ')
+    if id_name.isdigit():
+        req = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale). \
+            filter(Sale.id_stock == Stock.id).filter(Shop.id == Stock.id_shop). \
+            filter(Book.id == Stock.id_book).filter(Publisher.id == Book.id_publisher).\
+            filter(Publisher.id == id_name)
+        for с in req.all():
+            print(f' {с.title:<40}|  {с.name: <10}| {с.price:<5} | {с.date_sale}')
+    else:
+        req = session.query(Shop.name, Book.title, Sale.price, Sale.date_sale). \
+            filter(Sale.id_stock == Stock.id).filter(Shop.id == Stock.id_shop). \
+            filter(Book.id == Stock.id_book).filter(Publisher.id == Book.id_publisher). \
+            filter(Publisher.name.like(f'%{id_name}%'))
+        for с in req.all():
+            print(f' {с.title:<40}|  {с.name: <10}| {с.price:<5} | {с.date_sale}')
+
 
 session.close()
+
+
+if __name__ == '__main__':
+    load_json()
+    get_data()
